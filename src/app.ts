448668e4dbe75854f93eaba111b3622389953d2f
renangiaretta <renan.giaretta@hotmail.com>
2023-06-04 20:03:00 -0300

    fix: use cors on app

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,11 @@ import { customersRoutes } from './routes/customers.routes'
 import { handleAppErrorMiddleware } from './middlewares/handleAppError.middleware'
 import { loginRoutes } from './routes/login.routes'
 import { contactsRoutes } from './routes/contacts.routes'
+import cors from 'cors'
 
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 app.use('/login', loginRoutes)
 app.use('/customers', customersRoutes)
